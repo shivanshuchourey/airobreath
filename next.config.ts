487b00f0a125226aab4a3309fa02222f2e8b1175
Shivanshu Chourey <Shivanshuchoureysc21@gmail.com>
2026-01-16 09:50:27 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,10 @@ const nextConfig: NextConfig = {
   /* config options here */
   images: {
     remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      },
     ],
   },
 };
